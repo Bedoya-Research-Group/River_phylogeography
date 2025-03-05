5b644179b6f9bb00e42e050665d04e12d4769eff
ambed0ya <abedoya@nybg.org>
2025-03-05 08:32:57 -0500

    file edited

--- 1_trimming.sh
+++ 1_trimming.sh
@@ -1,6 +1,6 @@
-for infile in /media/ambedoya/extradrive1/Lagomarsino/Palicourea/RAW/*R1_001.fastq.gz
+for infile in /Volumes/Bedoya/Genome/*R1.fastq.gz
 	do
-	base=$(basename ${infile} R1_001.fastq.gz)
-	trimmomatic PE -threads 15 ${infile} ${base}R2_001.fastq.gz ${base}R1_001.trim.fq.gz ${base}R1_001un.trim.fq.gz ${base}R2_001.trim.fq.gz ${base}R2_001un.trim.fastq.gz ILLUMINACLIP:all_adapters.fasta:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+	base=$(basename ${infile} R1.fastq.gz)
+	trimmomatic PE -threads 15 ${infile} ${base}R2.fastq.gz ${base}R1.trim.fq.gz ${base}R1_un.trim.fq.gz ${base}R2.trim.fq.gz ${base}R2_un.trim.fastq.gz ILLUMINACLIP:adapters.fasta:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
 	done
 
